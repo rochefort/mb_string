023c70fb924c1caddb9bea9a97e750665152f4a3
rochefort <terasawan@gmail.com>
2015-11-25 04:35:42 +0900

    Add `mb_center` tests

--- spec/core_ext/string_spec.rb
+++ spec/core_ext/string_spec.rb
@@ -139,4 +139,77 @@ describe 'String' do
       end
     end
   end
+
+  describe '#mb_center' do
+    context 'without pad_str' do
+      context 'ascii-strings' do
+        examples = [
+          { object: 'abc', inputs: 2, expected: 'abc' },
+          { object: 'abc', inputs: 3, expected: 'abc' },
+          { object: 'abc', inputs: 4, expected: 'abc ' },
+          { object: 'abc', inputs: 5, expected: ' abc ' },
+          { object: 'abc', inputs: 6, expected: ' abc  ' },
+        ]
+        include_examples 'without_pad_str', 'mb_center', examples
+      end
+    end
+
+    context 'multibytes-strings' do
+      examples = [
+        { object: 'あいう', inputs: 5, expected: 'あいう' },
+        { object: 'あいう', inputs: 6, expected: 'あいう' },
+        { object: 'あいう', inputs: 7, expected: 'あいう ' },
+        { object: 'あいう', inputs: 8, expected: ' あいう ' },
+      ]
+      include_examples 'without_pad_str', 'mb_center', examples
+    end
+  end
+
+  context 'with pad_str' do
+    context 'ascii-strings' do
+      examples = [
+        { object: 'abc', inputs: [2, 'x'], expected: 'abc' },
+        { object: 'abc', inputs: [3, 'x'], expected: 'abc' },
+        { object: 'abc', inputs: [4, 'x'], expected: 'abcx' },
+        { object: 'abc', inputs: [5, 'x'], expected: 'xabcx' },
+
+        { object: 'abc', inputs: [4, 'xy'], expected: 'abcx' },
+        { object: 'abc', inputs: [5, 'xy'], expected: 'xabcx' },
+        { object: 'abc', inputs: [6, 'xy'], expected: 'xabcxy' },
+        { object: 'abc', inputs: [7, 'xy'], expected: 'xyabcxy' },
+        { object: 'abc', inputs: [8, 'xy'], expected: 'xyabcxyx' },
+        { object: 'abc', inputs: [9, 'xy'], expected: 'xyxabcxyx' },
+      ]
+      include_examples 'with_pad_str', 'mb_center', examples
+    end
+
+    context 'multibytes-strings' do
+      examples = [
+        { object: 'あいう', inputs: [5, 'x'], expected: 'あいう' },
+        { object: 'あいう', inputs: [6, 'x'], expected: 'あいう' },
+        { object: 'あいう', inputs: [7, 'x'], expected: 'あいうx' },
+        { object: 'あいう', inputs: [8, 'x'], expected: 'xあいうx' },
+        { object: 'あいう', inputs: [9, 'x'], expected: 'xあいうxx' },
+
+        { object: 'あいう', inputs: [5, 'え'], expected: 'あいう' },
+        { object: 'あいう', inputs: [6, 'え'], expected: 'あいう' },
+        { object: 'あいう', inputs: [7, 'え'], expected: 'あいう ' },
+        { object: 'あいう', inputs: [8, 'え'], expected: ' あいう ' },
+        { object: 'あいう', inputs: [9, 'え'], expected: ' あいうえ' },
+        { object: 'あいう', inputs: [10, 'え'], expected: 'えあいうえ' },
+
+        { object: 'あいう', inputs: [5, 'えお'], expected: 'あいう' },
+        { object: 'あいう', inputs: [6, 'えお'], expected: 'あいう' },
+        { object: 'あいう', inputs: [7, 'えお'], expected: 'あいう ' },
+        { object: 'あいう', inputs: [8, 'えお'], expected: ' あいう ' },
+        { object: 'あいう', inputs: [9, 'えお'], expected: ' あいうえ' },
+        { object: 'あいう', inputs: [10, 'えお'], expected: 'えあいうえ' },
+        { object: 'あいう', inputs: [11, 'えお'], expected: 'えあいうえ ' },
+        { object: 'あいう', inputs: [12, 'えお'], expected: ' えあいうえ ' },
+        { object: 'あいう', inputs: [13, 'えお'], expected: ' えあいうえお' },
+        { object: 'あいう', inputs: [14, 'えお'], expected: 'えおあいうえお' },
+      ]
+      include_examples 'with_pad_str', 'mb_center', examples
+    end
+  end
 end
